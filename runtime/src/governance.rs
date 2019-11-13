c7278fff602ea3f803807d095b03dd92afb58089
minamide <minamide@shibanoBookpuro.AirPort>
2019-11-13 17:14:23 +0900

    cast vote function

--- runtime/src/governance.rs
+++ runtime/src/governance.rs
@@ -1,7 +1,7 @@
 use support::{decl_module, decl_storage, decl_event, dispatch::Result, ensure };
 use system::ensure_signed;
 use codec::{Encode, Decode};
-
+use rstd::prelude::Vec;
 
 // Option: {title: String, pot: u64, voters: <Vec:T::AccountId>}
 // Voter: {accountId, votedVotes:<Vec: u64>, timeLastVoted: timestamp, balance: balances}
@@ -11,26 +11,26 @@ use codec::{Encode, Decode};
 pub struct Vote<AccountId, Timestamp> {
     id: u64,
     creator: AccountId,
-    // index: u64,
-    // id: Hash,
+    when: Timestamp, //T::Moment
+    vote_type: u8,
+    aye: Vec<AccountId>,
+    nay: Vec<AccountId>, //or :map u64 -> AccountId   vec.len().push()
     // enum Vote_type　{
     //     Majority Rule, // Majority Rule(expire, threshold: {adaptive quorum})
     //     Stake Locking, // Stake Locking 
     //     Quadratic Voting
-    // },
-    // option: map u8 => String
-    aye: u64,
-    nay: u64,
-    when: Timestamp, //T::Moment
+    // }
+
     // expireblock: 
 }
 
 
-// #[derive(PartialEq, Eq, RuntimeDebug)]
-// pub enum Ballot {
-//     Aye,
-//     Nay
-// }
+#[derive(Clone, Copy, PartialEq, Eq, Encode, Decode)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub enum Ballot {
+    Aye,
+    Nay,
+}
 
 // import Trait from balances, timestamp, event
 pub trait Trait: balances::Trait + timestamp::Trait + system::Trait {
@@ -40,7 +40,8 @@ pub trait Trait: balances::Trait + timestamp::Trait + system::Trait {
 decl_event!(
 	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
         //created, voted, withdrawn
-		Created(AccountId, u64),
+        Created(AccountId, u64),
+        Voted(AccountId, u64, Ballot),
 	}
 );
 
@@ -55,7 +56,7 @@ decl_storage! {
         VoteCreator get(creator_of): map u64 => Option<T::AccountId>;
         CreatedVoteCount get(created_by): map T::AccountId => u64; // increment everytime created
         // VoteByCreatorArray get(created_by): map T::AccountId => <Vec: u64>;
-        VoteByCreatorArray get(created_by_and_index): map (T::AccountId, u64) => u64;
+        VoteByCreatorArray get(created_by_and_index): map (T::AccountId, u64) => Vote<T::AccountId, T::Moment>;
     }
     // VoteByVoter: map T::AccountId => <Vec: u64>;
 }
@@ -68,59 +69,58 @@ decl_module! {
         // create_vote_majority_rule(Title, Option(comma separated), Expiretime)
         fn create_vote(origin) -> Result {
             let sender = ensure_signed(origin)?;
-            let vote_num = <AllVoteCount>::get();
             let new_vote_num = <AllVoteCount>::get().checked_add(1)
                 .ok_or("Overflow adding vote count")?;
             let vote_count_by_sender = <CreatedVoteCount<T>>::get(sender.clone()).checked_add(1)
                 .ok_or("Overflow adding vote count to the sender")?;
 
             let new_vote = Vote{
-                id: vote_num,
+                id: new_vote_num,
+                vote_type: 0,
                 creator: sender.clone(),
                 when: <timestamp::Module<T>>::get(),
-                aye: 0,
-                nay: 0,
+                aye: Vec::new(), //0　!vec[]
+                nay: Vec::new() //::new(),
             };
 
-            Self::mint_vote(sender, vote_num, new_vote, vote_count_by_sender, new_vote_num)?;
+            Self::mint_vote(sender, new_vote, vote_count_by_sender, new_vote_num)?;
         
             Ok(())
         }
-        
+
         //TODO: add voter modules: {vote}
-        // pub fn cast_ballot(origin, reference_index, vote: Ballot) Result -> {
-        //     let voter = ensure_signed(origin)?;
-        //     ensure!(<VotesByIndex<T>>::exists(&reference_index), "Vote doesn't exists");
-        //     // ensure the vote index exists
-        //     // ensure the voter hasnt voted in the vote
+        fn cast_ballot(origin, reference_index: u64, ballot: Ballot) -> Result {
+            let sender = ensure_signed(origin)?;
+            ensure!(<VotesByIndex<T>>::exists(&reference_index), "Vote doesn't exists");
+            // ensure the voter hasnt voted in the vote or change it to the other
             
-        //     // increment aye or nay
-        //     match vote {
-        //         Ballot::aye -> {
-        //             <VotesByIndex>::get(reference_index).nay.checked_add(1)
-        //                 .ok_or("Overflow adding vote count")?;
-        //         }
-        //         Ballot::nay -> {
-        //             <VotesByIndex>::get(reference_index).nay.checked_add(1)
-        //                 .ok_or("Overflow adding vote count")?;
-        //         }
-        //     }
-        // }
-        
+            // push voter id to aye or nay
+            match ballot {
+                Ballot::Aye => {
+                    <VotesByIndex<T>>::get(reference_index).aye.push(sender.clone());
+                    Self::deposit_event(RawEvent::Voted(sender, reference_index, ballot));
+                }
+                Ballot::Nay => {
+                    <VotesByIndex<T>>::get(reference_index).nay.push(sender.clone());
+                    Self::deposit_event(RawEvent::Voted(sender, reference_index, ballot));
+                }
+            }
+            Ok(())
+        }
     }
 }
 
 impl<T: Trait> Module<T> {
-    fn mint_vote(sender: T::AccountId, vote_num: u64, new_vote: Vote<T::AccountId, T::Moment>, vote_count_by_sender: u64, new_vote_num: u64 ) -> Result{
-        ensure!(!<VotesByIndex<T>>::exists(&vote_num), "Vote already exists");
+    fn mint_vote(sender: T::AccountId, new_vote: Vote<T::AccountId, T::Moment>, vote_count_by_sender: u64, new_vote_num: u64 ) -> Result{
+        ensure!(!<VotesByIndex<T>>::exists(&new_vote_num), "Vote already exists");
 
-        <VotesByIndex<T>>::insert(vote_num.clone(), new_vote);
-        <VoteCreator<T>>::insert(vote_num.clone(), sender.clone());
+        <VotesByIndex<T>>::insert(new_vote_num.clone(), &new_vote);
+        <VoteCreator<T>>::insert(new_vote_num.clone(), sender.clone());
         <CreatedVoteCount<T>>::insert(sender.clone(), vote_count_by_sender);
-        <VoteByCreatorArray<T>>::insert((sender.clone(), vote_count_by_sender), vote_num.clone());
+        <VoteByCreatorArray<T>>::insert((sender.clone(), vote_count_by_sender), new_vote);
         <AllVoteCount>::put(new_vote_num);
 
-        Self::deposit_event(RawEvent::Created(sender, vote_num));
+        Self::deposit_event(RawEvent::Created(sender, new_vote_num));
 
         Ok(())
     }
