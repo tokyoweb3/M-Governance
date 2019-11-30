89b0dfa2b71d5e496571899897a81e6b951dc221
minamide <minamide@shibanoBookpuro.AirPort>
2019-11-30 20:49:25 +0900

    add function to add new account

--- runtime/src/mynumber.rs
+++ runtime/src/mynumber.rs
@@ -9,21 +9,23 @@ use rstd::prelude::*;
 use support::{dispatch::Result, decl_storage, decl_module, decl_event, ensure, print};
 use system::{ensure_signed};
 use codec::{Encode, Decode};
-use sr_primitives::traits::{SaturatedConversion};
-use primitives::{U256};
+use sr_primitives::traits::{};
+use primitives::{};
 
 pub trait Trait: system::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 pub type RSA = Vec<u8>; //2048bit
+pub type Pubkey = Vec<u8>; //2048bits
+
 
 #[derive(PartialEq, Encode, Decode, Default)]
 #[cfg_attr(feature = "std", derive(Debug))]
-pub struct Certification<RSA> {
-    pubkey: RSA,    // mynumber pubkey
-    encrypted_pubkey: RSA,  // mynumber encrypted by the government
-    encrypted_account: RSA,     // account encrypted with pubkey
+pub struct Certification<Pubkey, Hash> {
+    pubkey: Pubkey,    // mynumber pubkey
+    cert: Hash,  // mynumber signed by CA
+    encrypted_account: Hash,     // account encrypted with pubkey
 }
 
 decl_event!(
@@ -36,7 +38,7 @@ decl_storage! {
     trait Store for Module<T: Trait> as MyNumber {
         AccountCount get(accounts_count): u64;
         // Public Key => MyNumber, Signed Hashed of Public Key with MyNumberPublicKey: 
-        AccountStore: map T::AccountId => Certification<RSA>;
+        AccountStore: map T::AccountId => Certification<Pubkey, T::Hash>;
         
         // My Number => Hash of mynumber signed by Government
         // MyNumberStore get(signature): RSA => RSA;
@@ -48,18 +50,16 @@ decl_module! {
         fn deposit_event() = default;
 
         // register
-        pub fn register_account(origin, pubkey: RSA, privkey: RSA, encrypted_pubkey: RSA) -> Result{
+        pub fn register_account(origin, pubkey: Pubkey, encrypted_account: T::Hash, cert: T::Hash) -> Result{
+            ensure!(pubkey.len() = 256, "RSA public key should be 256 bytes");
+
             let sender = ensure_signed(origin)?;
             ensure!(!<AccountStore<T>>::exists(&sender), "Your account is already registered.");
             let id = Self::accounts_count();
 
-            let data = &sender.encode();
-            // encrypt sender account => result
-            let encrypted_account = encrypt(data, &privkey).unwrap();
-
             let certificate = Certification {
                 pubkey,
-                encrypted_pubkey,
+                cert,
                 encrypted_account,
             };
             
@@ -71,7 +71,14 @@ decl_module! {
         }
 
         // verify
-        
+        pub fn get_account(origin) -> Result {
+            let sender = ensure_signed(origin)?;
+            let key1: T::AccountId = sender;
+            let key1_as_vec: Vec<u8> = key1.encode();
+            print(&key1_as_vec[..]);
+            Ok(())
+            // d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
+        }
         // update
 
         // revoke
@@ -86,15 +93,18 @@ mod tests {
     use super::*;
     use crate::rsa::*;
     use support::{
-        impl_outer_origin, assert_ok, assert_noop, parameter_types,
+        impl_outer_origin, assert_ok, assert_noop, parameter_types
     };
     use runtime_io::{TestExternalities};
     use sr_primitives::{
-        Perbill, traits::{IdentityLookup},
+        Perbill, traits::{IdentityLookup, Hash, Member, },
         testing::{Header}
     };
     use primitives::{H256};
-    
+    // extern crate rustc_hex;
+    use rustc_hex::{FromHex, ToHex};
+    // use rustc_serialize::base64::{ToBase64, FromBase64, STANDARD};
+
     impl_outer_origin! {
         pub enum Origin for Test {}
     }
@@ -135,44 +145,42 @@ mod tests {
     }
 
     type MyNumber = Module<Test>;
-
+    
     //public modulus
-    const PUBLIC_KEY: [u8; 32] = [
-        159, 152, 51, 63, 56, 236, 171, 124,
-        45, 135, 54, 162, 205, 236, 198, 245,
-        19, 46, 53, 100, 118, 84, 91, 52,
-        154, 205, 76, 225, 199, 53, 134, 136
-    ];
-
-    //private exponent
-    const PRIVATE_KEY: [u8; 32] = [
-        25, 179, 118, 205, 152, 40, 219, 84,
-        40, 144, 120, 121, 145, 37, 130, 26,
-        36, 45, 66, 62, 172, 151, 163, 62,
-        196, 188, 207, 172, 93, 93, 87, 81
-    ];
-
     #[test]
     fn can_register() {
         TestExternalities::default().execute_with(||{
-            let sender: u64 = 1997;
-
             // register account
-            assert_ok!(MyNumber::register_account(Origin::signed(sender), PUBLIC_KEY.to_vec(), PRIVATE_KEY.to_vec(), PRIVATE_KEY.to_vec()));
+            let pub_str: String = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArtIADvVx7dIsMwZe60Fgb3cB2wQXo372wXdafJlZnA0ZwhCd9CxrRmlYCvq86c6hYtF/nBDjjNBvvzGPgain+KwZkiM5mHOKV7phACEvt6pGCjTza4HPEQ+VX+Lk+T5WvkXvSQ4WMa0cQ6GA4A39TG/2d2P7U9bhm/DW2A0bd8gUVb7O171zcX49cq1bHidUNu3rBKqcTQESvaQCNKtiHtqK9WBrkcAtl3w0vBNEcyRM9cVRpzQCZF6N+H3KToXccroIbgJvjcx5rZsaZZrMFJsLY7eFbWU3wJ7RDT8+LhEtbHRelyBU0XhGekqIELibOWEmIa8qcNFjf1Zz/yLNHQIDAQAB".as_bytes().to_hex();
+            // let pubkey: Vec<u8> = b"0xAED2000EF571EDD22C33065EEB41606F7701DB0417A37EF6C1775A7C99599C0D19C2109DF42C6B4669580AFABCE9CEA162D17F9C10E38CD06FBF318F81A8A7F8AC1992233998738A57BA6100212FB7AA460A34F36B81CF110F955FE2E4F93E56BE45EF490E1631AD1C43A180E00DFD4C6FF67763FB53D6E19BF0D6D80D1B77C81455BECED7BD73717E3D72AD5B1E275436EDEB04AA9C4D0112BDA40234AB621EDA8AF5606B91C02D977C34BC134473244CF5C551A73402645E8DF87DCA4E85DC72BA086E026F8DCC79AD9B1A659ACC149B0B63B7856D6537C09ED10D3F3E2E112D6C745E972054D178467A4A8810B89B39612621AF2A70D1637F5673FF22CD1D".to_vec();
+            let pubkey: Vec<u8> = pub_str.from_hex().unwrap();
+            let data: Vec<u8> = [22, 33, 111, 112].to_vec();
+            assert_eq!(data, pubkey);
+            // let cert: H256 = H256::from_slice(b"0x290cd4dc09c152b574df6e1834335d5ddb5326e1450c2501b471aa6980ed7065");
+            // let signed_account: H256 = H256::from_slice(b"0x7a1d6c873fb9d4114e9806b4c4ad3a49d2146cbe31a87b0d15c1f38ebbfee93c");
+        
+            // let cert: H256 = sr_primitives::traits::Hash::hash(&[444, 555, 66, 777]);
+            let cert = sr_primitives::traits::BlakeTwo256::hash(&[111, 112, 113, 114]);
+            let signed_account = sr_primitives::traits::BlakeTwo256::hash(&[122, 122, 122, 122]);
 
-            // encode account to byte array
-            let account = &sender.encode();
+            assert_ok!(MyNumber::register_account(Origin::signed(1), pubkey, cert, signed_account));
 
-            // encrypt account with private key
-            let encrypted_account = encrypt(account, &PRIVATE_KEY).unwrap();
-
-            let cert = Certification {
-                pubkey: PUBLIC_KEY.to_vec(),
-                encrypted_pubkey: PRIVATE_KEY.to_vec(),
-                encrypted_account,
-            };
+            // let certification = Certification {
+            //     pubkey,
+            //     cer,
+            //     encrypted_account: signed_account,
+            // };
             
-            assert_eq!(<AccountStore<Test>>::get(&sender), cert);
+            // assert_eq!(<AccountStore<Test>>::get(1), certification);
         });
     }
+
+    // const PUBLIC_KEY_2048 = "551322645510721241055120100471111112304112510522620103551322653111502426455111204445111022344055215034265547712166455310120430167101214425211012064750111520234121103204445111012064554716220242521104202445646736053166115346445446631255167123164306010230631542130242734626215631557141310541671323304514613214040556144206641671623604147113133066522703427310315015461466572147213115221041156166204471521202167516615632260547167226254121153226553211722066465160156531131052064055016015472166124324415071101506057253242425201141265412665250255531303267312764242515666030246527107154505436320232101107250050716231031057712523352057332641421011445350470310610601422545254767142474671303067506116130634464151220610611652345254411320471063121250615611413545150511323441521110024645647122670564153344411271543504054316614073463143212471027123251171160244531431322065057253234331061242263151014330654157142222675621523544514716215274143132302715321223271132114346451061063123353163252535421221564516753160521041642126411414524444142125204745541073205406011134265545142322461051552125351716116060511152214471431722643054611623074457111242440121012424050455132051021051322645512010042116111230411251052262010355132265312426455";
+    // #[test]
+    // fn hex_to_vec(){
+    //     TestExternalities::default().execute_with(||{
+
+            
+    //     });
+    // }
 }
